f599e9c3219945638ea8514e3fad5479d91a0012
DiamondLevelTraining <johnsechen12@gmail.com>
2025-04-23 22:17:00 -0500

    Delete src directory

--- src/src/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>Diamond Level Training</h1>
-      <p>Unlock Your Full Potential – Baseball, Softball & Fitness Coaching</p>
-    </div>
-  );
-}
